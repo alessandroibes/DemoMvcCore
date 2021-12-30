d02507329487b9b89ef69dcbf918c282978df52b
Alessandro Oliveira <alessandro.ibes@gmail.com>
2021-12-30 13:10:27 -0300

    Adicionado rotas

--- DemoMvc/Controllers/HomeController.cs
+++ DemoMvc/Controllers/HomeController.cs
@@ -1,26 +1,31 @@
 ﻿using DemoMvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DemoMvc.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(int id, Guid categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
