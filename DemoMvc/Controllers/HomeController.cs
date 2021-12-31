cbe1d427d3a04a499bde1865668e5b2b35f8f1a4
Alessandro Oliveira <alessandro.ibes@gmail.com>
2021-12-31 10:11:45 -0300

    Adicionado Model, PartilView e ViewComponent

--- DemoMvc/Controllers/HomeController.cs
+++ DemoMvc/Controllers/HomeController.cs
@@ -2,30 +2,23 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace DemoMvc.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
-        public IActionResult Index(int id, Guid categoria)
+        public IActionResult Index()
         {
             return View();
         }
 
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
-        public IActionResult Privacy()
+        public IActionResult Privacy(Filme filme)
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ DemoMvc/Models/Filme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoMvc.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório")]
+        [Display(Name = "Título")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O título precisa ter entre 3 e 60 caracteres")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [Required(ErrorMessage = "O campo Gênero é obrigatório")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF]""'\w-]*$", ErrorMessage = "Gênero em formato inválido")]
+        [StringLength(30, ErrorMessage = "Máximo de 30 caracteres")]
+        [Display(Name = "Gênero")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O campo Valor é obrigatório")]
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "O campo Avaliação é obrigatório")]
+        [Display(Name = "Avaliação")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        public int Avaliacao { get; set; }
+    }
+}
