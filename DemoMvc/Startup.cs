56db5ef57171b133f4538a8099899b005ce13086
Alessandro Oliveira <alessandro.ibes@gmail.com>
2021-12-31 12:37:41 -0300

    Adicionados controller e views usando Scaffold

--- DemoMvc/Startup.cs
+++ DemoMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoMvc.Data;
 
 namespace DemoMvc
 {
@@ -33,6 +35,9 @@ namespace DemoMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<DemoMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DemoMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
