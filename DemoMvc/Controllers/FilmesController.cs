a20efc95da24540647ef33df74f332832badddff
Alessandro Oliveira <alessandro.ibes@gmail.com>
2021-12-31 11:26:17 -0300

    Adicionado formulário com validação

---
+++ DemoMvc/Controllers/FilmesController.cs
@@ -0,0 +1,28 @@
+﻿using DemoMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMvc.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            { 
+            }
+
+            return View(filme);
+        }
+    }
+}
